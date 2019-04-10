c9833752d3d923bf1583ddc05d799d135ec85712
Junior Dugue <jrdugue@gmail.com>
2019-04-10 16:07:41 -0400

    deleted unnceccary cra info

--- Ch02/02_02/start/react-is-fun/src/index.js
+++ Ch02/02_02/start/react-is-fun/src/index.js
@@ -1,18 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-class Message extends React.Component {
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        <h1 style={{color: this.props.color}}>
-        {this.props.msg}</h1>
-        <p>I'll check back in {this.props.minutes} minutes.</p>
-      </div>
-    )
-  }
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render( <Message msg='how are you?' age={50} color="blue" minutes={50}/>, document.getElementById('root')
-)
