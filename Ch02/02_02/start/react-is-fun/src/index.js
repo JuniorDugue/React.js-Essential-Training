a468fa66c8b9ae15220a992dc8db56077112b585
Junior Dugue <jrdugue@gmail.com>
2019-04-10 14:30:59 -0400

    forked project

---
+++ Ch02/02_02/start/react-is-fun/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Message extends React.Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h1 style={{color: this.props.color}}>
+        {this.props.msg}</h1>
+        <p>I'll check back in {this.props.minutes} minutes.</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render( <Message msg='how are you?' age={50} color="blue" minutes={50}/>, document.getElementById('root')
+)
