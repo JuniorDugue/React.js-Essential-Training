eb5eed45ba5867ae70c67488e0bba4e06b6432d1
Junior Dugue <jrdugue@gmail.com>
2019-04-11 14:11:40 -0400

    learned when passing a number, to wrap it in an expression, practice more with props and passing data

---
+++ Ch02/02_02/start/react-is-fun/src/components/Classpractice.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Classpractice extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Class Practice</h2>
+        <p>Practicing class components structure</p>
+      </>
+    )
+  }
+}
+
+export default Classpractice;
